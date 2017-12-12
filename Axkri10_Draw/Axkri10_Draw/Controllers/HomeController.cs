065a6e8639d96b01b3e5c7b80c7dd5365ca7103c
axkri <axkri10@student.sdu.dk>
2017-12-12 13:21:19 +0100

    HomeController, SubFormModel and XmlSerialzation made.

--- Axkri10_Draw/Axkri10_Draw/Controllers/HomeController.cs
+++ Axkri10_Draw/Axkri10_Draw/Controllers/HomeController.cs
@@ -1,21 +1,52 @@
-﻿using System;
+﻿using Axkri10_Draw.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Serialization;
 
 namespace Axkri10_Draw.Controllers
 {
     public class HomeController : Controller
     {
+        string pathString = "~/App_Data/submissions.xml";
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+         public ActionResult Index(SubFormModel submodel)
+        {
+            string firstName = submodel.FirstName;
+            string surName = submodel.SurName;
+            string email = submodel.Email;
+            int phonenumber = submodel.PhoneNumber;
+            int birthday = submodel.Birthday;
+            int serialNo = submodel.SerialNo;
+            
+            ViewBag.Demo = submodel;
+
+            string pathToFolder = Server.MapPath(pathString);
+
+            XmlSerialization.WriteToXmlFile<SubFormModel>(pathToFolder, submodel);
+
+
+            return View();
+        }
+
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+
+            string pathToFolder = Server.MapPath(pathString);
+
+            List<SubFormModel> submissions = XmlSerialization.ReadFromXmlFile<List<SubFormModel>>(pathToFolder);
+
+            ViewBag.Display = submissions;
 
             return View();
         }
---
+++ Axkri10_Draw/Axkri10_Draw/Models/SubFormModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Axkri10_Draw.Models
+{
+    [Serializable]
+    public class SubFormModel
+    {
+        private SubFormModel person;
+
+        public String FirstName { get; set; }
+        public String SurName { get; set; }
+        public String Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Birthday { get; set; }
+        public int SerialNo { get; set; }
+
+        public SubFormModel() { }
+
+        public SubFormModel(String firstName, String surName, String email, int phoneNumber, int birthday, int serialNo)
+        {
+            FirstName = firstName;
+            SurName = surName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Birthday = birthday;
+            SerialNo = serialNo;
+        }
+
+        public SubFormModel(SubFormModel person)
+        {
+            this.person = person;
+        }
+
+        public override string ToString()
+        {
+
+            return String.Format("FirstName: " + FirstName + " SurName: " + SurName + " Email:" + Email +
+                " PhoneNumber: " + PhoneNumber + " Birthday: " + Birthday + " SerialNo: " + SerialNo);
+
+        }
+
+    }
+}
